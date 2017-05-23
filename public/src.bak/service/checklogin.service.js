2f1e41d557c7edbf8cc7fbedfd7bdc94c8ea27f9
sxmz <huangliangk@163.com>
2017-05-23 14:51:53 +0800

    删除目录public/src.bak

--- public/src.bak/service/checklogin.service.js
+++
@@ -1,6 +0,0 @@
-angular.module('asch').service('checkloginservice', function($http, $rootScope,$location,$window,ipCookie) {
-    // $rootScope.isLogin = false;
-    // if(!$rootScope.isLogin){
-    //     $window.location.href = '#/login'
-    // }
-});
