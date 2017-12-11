7f51cad033ba30e6adea0ce1f8f37986417c46bf
huangliangk <huangliangk@163.com>
2017-12-11 14:41:29 +0800

    update cli shell

--- cli/scripts/issue_asset.sh
+++ cli/scripts/issue_asset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly PROGRAM_DIR=$(readlink -m $(dirname $0))
+readonly PROGRAM_DIR=$(dirname $0)
 readonly CLI=$PROGRAM_DIR/../bin/asch-cli
 
 $CLI issueasset -e "dinner mention gadget chief embody neglect sort gentle trial pitch rice reopen" -c "PuerBank.PEB" -a 10000000 -r 0.2
